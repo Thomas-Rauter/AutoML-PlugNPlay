8f2eb9ed7fc2931226bcffc47e480bb02889f8f5
thomasrauter <rauterthomas0@gmail.com>
2023-11-25 18:01:47 +0100

    Dockerized a lot.

--- run_nn_hyperpara_screener_container.sh
+++ run_nn_hyperpara_screener_container.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Assigning command line arguments to variables
-local_train_dataset=$1
-local_dev_dataset=$2
-
 # Check if the nn_hyperpara_screener image is available locally
 if [[ "$(docker images -q nn_hyperpara_screener 2> /dev/null)" == "" ]]; then
   echo "Image not found locally. Pulling from Docker Hub..."
-  docker pull thomasrauter/auto_ml-hub:nn_hyperpara_screener
+  docker pull thomasrauter/auto_ml-hub:nn_hyperpara_screener:version_1
 fi
 
 # Running the Docker container with the provided arguments
-docker run -it --rm --name nn_hyperpara_container \
+docker run --rm --name nn_hyperpara_container \
+  -u "$(id -u):$(id -g)" \
   --volume "$(pwd)":/data \
-  nn_hyperpara_screener $local_train_dataset $local_dev_dataset
+  nn_hyperpara_screener "$@"
